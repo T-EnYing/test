c0bd49214f135d1704f77899786e4dbc5b848e95
Teng En Ying <tengenying@gmail.com>
2021-03-20 12:35:00 +0800

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Teng En Ying on 20/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var testLabel : UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        testLabel.text = "HelloWorld"
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
